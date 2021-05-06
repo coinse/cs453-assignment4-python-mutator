64c2a0d07426f8857885766464473f123c665b00
Sungmin Kang <stuatlittle@kaist.ac.kr>
2021-05-06 23:33:29 +0900

    removes mut flag necessary

--- pmut.py
+++ pmut.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Mutation Testing Tool.')
 	parser.add_argument('-a', '--action', choices=["mutate", "execute"], required=True)
 	parser.add_argument('-s', '--source', type=str, required=True)
-	parser.add_argument('-m', '--mutants', type=str, required=True)
+	parser.add_argument('-m', '--mutants', type=str, required=False)
 	parser.add_argument('-k', '--kill', type=str)
 
 	args = parser.parse_args()
